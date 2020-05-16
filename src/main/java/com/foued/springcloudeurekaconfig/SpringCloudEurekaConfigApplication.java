cf0d5d42b5faa6f85e48d642535a03eafd6258f7
fsliti <foued.sliti@prima-solutions.com>
2020-05-16 21:58:08 +0200

    Initial commit

---
+++ src/main/java/com/foued/springcloudeurekaconfig/SpringCloudEurekaConfigApplication.java
@@ -0,0 +1,15 @@
+package com.foued.springcloudeurekaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaConfigApplication.class, args);
+	}
+
+}
